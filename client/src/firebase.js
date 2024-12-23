3c319b98b5e29cc101f04742dbf24b65ee7361e0
vedantkesharia <keshariavedant@gmail.com>
2024-12-23 14:04:33 +0530

    commit 23rd dec

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,14 +4,28 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-estate-f8f18.firebaseapp.com",
-  projectId: "mern-estate-f8f18",
-  storageBucket: "mern-estate-f8f18.appspot.com",
-  messagingSenderId: "304767473129",
-  appId: "1:304767473129:web:e67df8378b70567db42778"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
+
+//old project works 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBzZXlBJuGkh1zpXpIFtXwZxDWb00YnsuA",
+//   authDomain: "fir-4-uploadfiles.firebaseapp.com",
+//   projectId: "fir-4-uploadfiles",
+//   storageBucket: "fir-4-uploadfiles.appspot.com",
+//   messagingSenderId: "567145842088",
+//   appId: "1:567145842088:web:23e5e367fe348662aac99d",
+//   measurementId: "G-42MH6HYYCY"
+// };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
